91df9a5c4f1c2f305966dc1580f48da072caa7ab
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2021-08-16 22:42:56 -0300

    ajustes no ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,26 @@
-console.log("✨DOTENV >>> "+process.env.DATABASE_URL);
+console.log("✨DOTENV >>> " + process.env.DATABASE_URL);
 module.exports = {
-  "type": "postgres",
-  "url" : process.env.DATABASE_URL,
-//   "host": "localhost",
-//   "port": 5432,
-//   "username": "postgres",
-//   "password": "postgres",
-//   "database": "simula_drive",
-  "entities": [
-    "./dist/models/*.js"
-  ],
-  "migrations": [
-    "./dist/database/migrations/*.js"
-  ],
-  "cli": {
-    "migrationsDir": "./src/database/migrations"
-  }
+    "type": "postgres",
+    "url": process.env.DATABASE_URL,
+    //   "host": "localhost",
+    //   "port": 5432,
+    //   "username": "postgres",
+    //   "password": "postgres",
+    //   "database": "simula_drive",
+    "dialectOptions": {
+        "ssl": {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    "entities": [
+        "./dist/models/*.js"
+    ],
+    "migrations": [
+        "./dist/database/migrations/*.js"
+    ],
+    "cli": {
+        "migrationsDir": "./src/database/migrations"
+    }
 }
 //"url" : "postgres://username:password@host:port/database"
