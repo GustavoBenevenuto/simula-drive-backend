845ae043c660bdb02e6efbf43b6579591ddf8dd0
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2021-09-30 19:01:26 -0300

    Validando corpo da requisição

--- src/routes/feedbackRouter.routes.ts
+++ src/routes/feedbackRouter.routes.ts
@@ -7,8 +7,11 @@ const feedbackRouter = Router();
 feedbackRouter.post('/', async (request, response) => {
     const { tipo_feedback, mensagem, avaliacao, id_questao } = request.body;
 
-    const feedback = { tipo_feedback, mensagem, avaliacao, id_questao };
+    if(tipo_feedback == undefined && mensagem == undefined && avaliacao == undefined && id_questao == undefined)
+        throw new AppError('O corpo da requisição está incorreto.',400); 
 
+    const feedback = { tipo_feedback, mensagem, avaliacao, id_questao };
+    
     const feedbackService = new FeedbackService();
     
     const resultado = await feedbackService.execute(feedback);
